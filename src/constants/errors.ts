9b17a2808a1f50a1b997b02a77fc540e1fdf0fe4
kononovzp <denys.kononov@dev.zenbit.tech>
2025-04-24 16:02:15 +0300

    add error message

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -4,6 +4,7 @@ export enum ErrorMessages {
   SomethingWentWrong = 'SomethingWentWrong',
   UnauthorizedException = 'UnauthorizedException',
   EmailOrPasswordInvalid = 'IncorrectEmailOrPasswordException',
+  UserAlreadyExists = 'UserAlreadyExistsException',
 }
 
 export const DEFAULT_ERROR_MESSAGE = ErrorMessages.SomethingWentWrong;
@@ -12,4 +13,5 @@ export const errorMessages: Record<string, TxKeyPath> = {
   [ErrorMessages.SomethingWentWrong]: 'errors.unknown',
   [ErrorMessages.UnauthorizedException]: 'errors.unauthorized',
   [ErrorMessages.EmailOrPasswordInvalid]: 'errorMessages.emailOrPasswordIncorrect',
+  [ErrorMessages.UserAlreadyExists]: 'errorMessages.emailAlreadyExist',
 };
